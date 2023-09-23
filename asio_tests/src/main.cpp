e079c70829fc4cb250c978834fc173a287f20967
slowcheetah <stas.varganov@yandex.ru>
2023-09-23 21:50:07 +0300

    introducing asio_test

---
+++ asio_tests/src/main.cpp
@@ -0,0 +1,34 @@
+#include <boost/asio.hpp>
+#include <iostream>
+
+using namespace std;
+
+void test()
+{
+    // Step 1. Assume that the client application has already
+    // obtained the IP-address and the protocol port number.
+    std::string raw_ip_address = "127.0.0.1";
+    unsigned short port_num = 3333;
+    // Used to store information about error that happens
+    // while parsing the raw IP-address.
+    boost::system::error_code ec;
+
+    // Step 2. Using IP protocol version independent address
+    // representation.
+    boost::asio::ip::address ip_address =
+            boost::asio::ip::address::from_string(raw_ip_address, ec);
+    if (ec.value() != 0) {
+        // Provided IP address is invalid. Breaking execution.
+        std::cout << "Failed to parse the IP address. Error code = " << ec.value()
+                  << ". Message: " << ec.message();
+        std::cout << ec.value() << std::endl;
+    }
+    // Step 3.
+    boost::asio::ip::tcp::endpoint ep(ip_address, port_num);
+    // Step 4. The endpoint is ready and can be used to specify a
+    // particular server in the network the client wants to
+    // communicate with.
+    return;
+}
+
+int main() { return 0; }
